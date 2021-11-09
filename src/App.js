c486f05efdade5f622bb5dbacc18ccdb07c5b64e
conrad96 <conrad.mugisha@gmail.com>
2021-11-09 20:49:45 +0300

    get term from state, setState videos list from api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Search from './components/Search';
+import Youtube from './api/Youtube';
+import './App.css'
 
-class App extends React.Component{    
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {videos: []}
+    }  
+
+    onTermSubmit = async term =>{
+        const response = await Youtube.get('/search', {
+            params: {
+                q: term
+            }
+        })
+
+        this.setState({videos: response.data.items})
+    }
 
     render(){
         return(
             <div className="container">
-                <Search />
+                <Search onFormSubmit={this.onTermSubmit} />
+                <div className="search-results-counter">
+                    {this.state.videos.length} results
+                </div>                
             </div>          
         );
     }
