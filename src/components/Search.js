0ac0228ac135531bd679c6cc3f30a43a99f294cb
conrad96 <conrad.mugisha@gmail.com>
2021-11-11 20:48:08 +0300

    imported font awesome icon to proj

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './Search.css';
 
 class Search extends React.Component{
@@ -21,7 +23,14 @@ class Search extends React.Component{
         return(
             <div className="search-field">
                 <form className="search-form" onSubmit={this.onFormSubmit} >
-                    <input type="text" onChange={evt=> this.searchField(evt) } className="form-control" id="search" placeholder="Search" value={this.state.term} />
+                    <div className="input-group">
+                        <input type="text" onChange={evt=> this.searchField(evt) } className="form-control" id="search" placeholder="Search" value={this.state.term} />
+                        <div className="input-group-append">
+                            <span className="input-group-text">
+                                <FontAwesomeIcon icon={faSearch} />
+                            </span>
+                        </div>
+                    </div>
                 </form>                
             </div>
         );
